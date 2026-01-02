f24da99688c5beeb7f36a9b500f07770e433b1a1
Ignatiusgoh <ignatiusgoh97@gmail.com>
2026-01-02 15:00:40 -0500

    adding market order test case

---
+++ test_cases/market_order.py
@@ -0,0 +1,19 @@
+import sys
+import os 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.indicator_cache import CandleCache
+from utils.trade_executer import BinanceFuturesTrader   
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+### TESTING PLACING MARKET ORDER ###
+
+trade = BinanceFuturesTrader()
+
+try:
+    response = trade.place_market_order(symbol="SOLUSDT", side="BUY", quantity=465.12)
+    print(response)
+except Exception as e:
+    print(f"Error: {e}")    
